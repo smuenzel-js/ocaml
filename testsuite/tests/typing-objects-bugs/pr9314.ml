31d5d6a61efd278babd3def6bc97e93093642ebf
Stefan Muenzel <smuenzel@janestreet.com>
2020-02-19 07:43:30 +0800

    Add fuel to try_expand_head

--- testsuite/tests/typing-objects-bugs/pr9314.ml
+++ testsuite/tests/typing-objects-bugs/pr9314.ml
@@ -97,3 +97,77 @@ val create :
                       alpha_of_gamma > >
   t = <fun>
 |}];;
+
+module type T = sig
+ type 'a alpha = 'b constraint 'a = < alpha : 'b >
+  type 'a beta = 'b constraint 'a = < beta : 'b >
+  type 'a gamma = 'b constraint 'a = < delta : 'c; gamma : 'b >
+  type 'a delta = 'b constraint 'a = < delta : 'b; gamma : 'c >
+  type 'a alpha_of_gamma = 'a gamma alpha
+  type 'a beta_of_delta = 'a delta beta
+  type ('a, 'b) w = W
+  type ('a, 'just_alpha) x = { field : ('a beta, 'just_alpha) w; }
+  type 'a t = A of ('a alpha_of_gamma, 'a beta_of_delta) w
+
+  val create :
+  ((< delta : < beta : 'a alpha_of_gamma >;
+      gamma : < alpha : 'a beta_of_delta > >
+    as 'a)
+   delta, 'a alpha_of_gamma)
+  x -> 'a t
+end
+[%%expect{|
+module type T =
+  sig
+    type 'a alpha = 'b constraint 'a = < alpha : 'b >
+    type 'a beta = 'b constraint 'a = < beta : 'b >
+    type 'a gamma = 'b constraint 'a = < delta : 'c; gamma : 'b >
+    type 'a delta = 'b constraint 'a = < delta : 'b; gamma : 'c >
+    type 'a alpha_of_gamma = 'a gamma alpha
+      constraint 'a = < delta : 'b; gamma : < alpha : 'c > >
+    type 'a beta_of_delta = 'a delta beta
+      constraint 'a = < delta : < beta : 'b >; gamma : 'c >
+    type ('a, 'b) w = W
+    type ('a, 'just_alpha) x = { field : ('a beta, 'just_alpha) w; }
+      constraint 'a = < beta : 'b >
+    type 'a t = A of ('a alpha_of_gamma, 'a beta_of_delta) w
+      constraint 'a = < delta : < beta : 'b >; gamma : < alpha : 'c > >
+    val create :
+      ((< delta : < beta : 'a alpha_of_gamma >;
+          gamma : < alpha : 'a beta_of_delta > >
+        as 'a)
+       delta, 'a alpha_of_gamma)
+      x -> 'a t
+  end
+|}, Principal{|
+module type T =
+  sig
+    type 'a alpha = 'b constraint 'a = < alpha : 'b >
+    type 'a beta = 'b constraint 'a = < beta : 'b >
+    type 'a gamma = 'b constraint 'a = < delta : 'c; gamma : 'b >
+    type 'a delta = 'b constraint 'a = < delta : 'b; gamma : 'c >
+    type 'a alpha_of_gamma = 'a gamma alpha
+      constraint 'a = < delta : 'b; gamma : < alpha : 'c > >
+    type 'a beta_of_delta = 'a delta beta
+      constraint 'a = < delta : < beta : 'b >; gamma : 'c >
+    type ('a, 'b) w = W
+    type ('a, 'just_alpha) x = { field : ('a beta, 'just_alpha) w; }
+      constraint 'a = < beta : 'b >
+    type 'a t = A of ('a alpha_of_gamma, 'a beta_of_delta) w
+      constraint 'a = < delta : < beta : 'b >; gamma : < alpha : 'c > >
+    val create :
+      ((< delta : < beta : 'a alpha_of_gamma >;
+          gamma : < alpha : 'a beta_of_delta > >
+        as 'a)
+       delta,
+       (< delta : < beta : 'b alpha_of_gamma >;
+          gamma : < alpha : 'b beta_of_delta > >
+        as 'b)
+       alpha_of_gamma)
+      x ->
+      (< delta : < beta : 'c alpha_of_gamma >;
+         gamma : < alpha : 'c beta_of_delta > >
+       as 'c)
+      t
+  end
+|}];;
